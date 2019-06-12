be9d156e60585ce495ce41e98fc3bc074c0ed57a
clghy <503773902@qq.com>
2019-06-12 18:54:49 +0800

    基于vue-cli3.0和elementUI整合PC端框架

---
+++ src/store/modules/app.js
@@ -0,0 +1,12 @@
+const app = {
+    state: {
+        name: 'vue_element_cli3',
+    },
+    mutations: {
+        SET_NAME: (state, name) => {
+            state.name = name
+        }
+    },
+}
+
+export default app
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{name}}
+  </div>
+</template>
+
+<script>
+import { mapGetters} from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'name',
+    ]),
+  },
+  data () {
+    return {
+      num:2
+    }
+  }
+}
+</script>
